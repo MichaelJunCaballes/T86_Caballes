1509b701921aae9b144562355da2da09c94bcfeb
MICHAEL JUN B. CABALLES <89232842+MichaelJunCaballes@users.noreply.github.com>
2021-09-18 05:50:18 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int age = 19;
+    printf("I am Michael Jun B. Caballes, Age of %d years old", age);
+
+    return 0;
+}
